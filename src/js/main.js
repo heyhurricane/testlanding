3a59022946da24e67ceec0421a4eabe2e2b3d95a
heyhurricane <dinohka@mail.ru>
2021-04-29 12:35:06 +0400

    fix some bugs in mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -36,28 +36,41 @@ const swiper3 = new Swiper('.feedback-swiper-container', {
 
 const resizeWindow = () => {
 
-  const changeText = (str) => {
-    const text = document.querySelector('.join__text');
+  const changeText = (section, str) => {
+    let text;
+    if (section === 'join') {
+      text = document.querySelector('.join__text');
+    }
+    else {
+      text = document.querySelector('.signup__text');
+    }
+
     text.innerHTML = str;
   };
 
   if (window.innerWidth <= 480) {
     const str = 'Удобно получать на емейл уникальные проверенные рецепты, которые дружная&nbsp;команда Tastycookery попробовала сама и рекомендует';
-    changeText(str);
+    changeText('join', str);
+    const str2 = 'Sign up to receive daily manually selected recipes, delicious weekly articles, 3-piece sets of slow cooker, vegetarian, and lunch ideas, plus Quick&Easy recipes and Nutrition Value flashcards via email.';
+    changeText('signup', str2);
   }
   else {
     const str = 'Удобно получать на емейл уникальные проверенные рецепты, которые&nbsp;дружная&nbsp;команда Tastycookery попробовала сама и рекомендует';
-    changeText(str);
+    changeText('join', str);
+    const str2 = 'Sign up to receive daily manually selected recipes, delicious weekly articles, 3&#8209;piece&nbsp;sets&nbsp;of slow cooker, vegetarian, and&nbsp;lunch ideas, plus Quick&Easy recipes and&nbsp;Nutrition Value flashcards via email.';
+    changeText('signup', str2);
   }
 
   window.addEventListener('resize', () => {
     if (window.innerWidth <= 480) {
       const str = 'Удобно получать на емейл уникальные проверенные рецепты, которые дружная&nbsp;команда Tastycookery попробовала сама и рекомендует';
-      changeText(str);
+      changeText('join', str);
     }
     else {
       const str = 'Удобно получать на емейл уникальные проверенные рецепты, которые&nbsp;дружная&nbsp;команда Tastycookery попробовала сама и рекомендует';
-      changeText(str);
+      changeText('join', str);
+      const str2 = 'Sign up to receive daily manually selected recipes, delicious weekly articles, 3&#8209;piece&nbsp;sets&nbsp;of slow cooker, vegetarian, and&nbsp;lunch ideas, plus Quick&Easy recipes and&nbsp;Nutrition Value flashcards via email.';
+      changeText('signup', str2);
     }
   });
 }
